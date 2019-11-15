f3724d7b93b65291d2366ac897147eeb560a0971
ryanperez <ryanperez473@gmail.com>
2019-11-15 20:47:07 +0800

    Initial upload

---
+++ CSBorrowingSystem/Adamson Computer Science Borrowing System.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSBorrowingSystem
+{
+    /// <summary>
+    /// Interaction logic for Adamson_Computer_Science_Borrowing_System.xaml
+    /// </summary>
+    public partial class Adamson_Computer_Science_Borrowing_System : Window
+    {
+        public Adamson_Computer_Science_Borrowing_System()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
+            int zOrder = Panel.GetZIndex(Accounts) + 1;
+            int zOrder2 = Panel.GetZIndex(BTransaction) + 1;
+            int zOrder3 = Panel.GetZIndex(Inventory) + 1;
+            int zOrder4 = zOrder + zOrder2 + zOrder3;
+
+            Panel.SetZIndex(Home, zOrder4);
+        }
+
+        private void BtnBTransact_Click(object sender, RoutedEventArgs e)
+        {
+            int zOrder = Panel.GetZIndex(Accounts) + 1;
+            int zOrder2 = Panel.GetZIndex(Home) + 1;
+            int zOrder3 = Panel.GetZIndex(Inventory) + 1;
+            int zOrder4 = zOrder + zOrder2 + zOrder3;
+
+            Panel.SetZIndex(BTransaction, zOrder4);
+        }
+
+        private void BtnInventory_Click(object sender, RoutedEventArgs e)
+        {
+            int zOrder = Panel.GetZIndex(Accounts) + 1;
+            int zOrder2 = Panel.GetZIndex(BTransaction) + 1;
+            int zOrder3 = Panel.GetZIndex(Home) + 1;
+            int zOrder4 = zOrder + zOrder2 + zOrder3;
+
+            Panel.SetZIndex(Inventory, zOrder4);
+        }
+
+        private void BtnAccounts_Click(object sender, RoutedEventArgs e)
+        {
+            int zOrder = Panel.GetZIndex(Inventory) + 1;
+            int zOrder2 = Panel.GetZIndex(BTransaction) + 1;
+            int zOrder3 = Panel.GetZIndex(Home) + 1;
+            int zOrder4 = zOrder + zOrder2 + zOrder3;
+
+            Panel.SetZIndex(Accounts, zOrder4);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CSBorrowingSystem/Inventory.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSBorrowingSystem
+{
+    /// <summary>
+    /// Interaction logic for Inventory.xaml
+    /// </summary>
+    public partial class Inventory : UserControl
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
